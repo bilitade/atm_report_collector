18277d7074c29720831f10400c197022576a58e0
Bilisuma <coop@DIG-BLP-10.coopbank.local>
2024-01-25 13:49:08 +0300

    atm information in atm_config.json  is corrected as requested  and some modifcation is done on log_config

--- log_config.py
+++ log_config.py
@@ -1,17 +1,29 @@
 import logging
 from logging import StreamHandler
 from logging.handlers import RotatingFileHandler
+from datetime import datetime
+import os
+
+# Determine the base destination folder and create the RunLog folder
+BASE_DESTINATION_FOLDER = r'C:\ATMLogs'
+RUN_LOG_FOLDER = os.path.join(BASE_DESTINATION_FOLDER, 'RunLog')
+os.makedirs(RUN_LOG_FOLDER, exist_ok=True)
 
 # Create a logger
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-# Create a rotating file handler, with a new file created at midnight and kept for 7 days
-log_file_handler = RotatingFileHandler('atm_log.txt', maxBytes=1024 * 1024, backupCount=7)
+# Create a rotating file handler, with a new file created for each run
+start_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+human_readable_start_time = datetime.now().strftime("%Y-%m-%d_%I-%M-%S%p")
+log_file_name = f'atm_log_{human_readable_start_time}.txt'
+log_file_path = os.path.join(RUN_LOG_FOLDER, log_file_name)
+
+log_file_handler = RotatingFileHandler(log_file_path, maxBytes=1024 * 1024, backupCount=0)  # backupCount=0 to keep only one log file
 log_file_handler.setLevel(logging.DEBUG)
 
-# Create a formatter for the file handler
-file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S\n')
+# Create a formatter for the file handler without including time
+file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%I-%M-%S%p %Y-%m-%d')
 log_file_handler.setFormatter(file_formatter)
 
 # Add the file handler to the logger
@@ -27,8 +39,9 @@ logger.addHandler(console_handler)
 
 def decorate_log_record(record):
     timestamp = record.created
-    decorative_timestamp = f"\n{'*' * 30}\n{'*' * 4} Log generated from a program run at {timestamp} {'*' * 4}\n{'*' * 30}\n"
-    return decorative_timestamp
+    human_readable_timestamp = datetime.utcfromtimestamp(timestamp).strftime('%I:%M:%S%p %Y-%m-%d %I:%M:%S%p')
+    decorative_timestamp = f"\n{'*' * 30}\n{'*' * 4} Log generated from a program run at {human_readable_timestamp} {'*' * 4}\n{'*' * 30}\n"
+    return decorative_timestamp + '\n\n \n'
 
 
 class DecorativeTimestampFormatter(logging.Formatter):
@@ -45,9 +58,8 @@ class DecorativeTimestampFormatter(logging.Formatter):
 
 
 decorative_formatter = DecorativeTimestampFormatter('%(asctime)s - %(levelname)s - %(message)s',
-                                                    datefmt='%Y-%m-%d %H:%M:%S')
+                                                    datefmt='%I:%M:%S%p %Y-%m-%d %I:%M:%S%p')
 
 # Set the formatter for both handlers
 log_file_handler.setFormatter(decorative_formatter)
 console_handler.setFormatter(decorative_formatter)
-
--- main.py
+++ main.py
@@ -9,6 +9,94 @@ from log_config import logger
 # Dictionary to store previous connections
 previous_connections = {}
 
+# def is_valid_ip(ip):
+#     try:
+#         socket.inet_aton(ip)
+#         return True
+#     except socket.error:
+#         return False
+
+# def connect_to_atm(atm):
+#     try:
+#         # Quick IP validation
+#         if not is_valid_ip(atm["ATM_IP"]):
+#             logger.error(f"Invalid IP address for ATM {atm['ATM_ID']}. Skipping.")
+#             return
+
+#         # Check if there is a previous connection
+#         if atm["ATM_ID"] in previous_connections:
+#             logger.info(f"Using existing connection for ATM {atm['ATM_ID']}")
+#         else:
+#             net_use_command = [
+#                 'net', 'use', fr'\\{atm["ATM_IP"]}\IPC$', f'/user:{atm["Domain"]}\\{atm["Username"]}', atm["Password"]
+#             ]
+#             subprocess.run(net_use_command, check=True, timeout=5)  # Set a shorter timeout of 5 seconds
+#             logger.info(f"Connected to ATM {atm['ATM_ID']}")
+
+#             # Save the connection in the dictionary
+#             previous_connections[atm["ATM_ID"]] = True
+
+#     except subprocess.CalledProcessError as e:
+#         logger.error(f"Authentication failed for ATM {atm['ATM_ID']} ({atm['ATM_IP']}): {e}")
+#         logger.info("Skipping to the next ATM.")
+#     except subprocess.TimeoutExpired as e:
+#         logger.error(f"Authentication failed or timeout for ATM {atm['ATM_ID']} ({atm['ATM_IP']}): {e}")
+#         logger.info("Skipping to the next ATM.")
+#     except Exception as e:
+#         logger.error(f"Error connecting to ATM {atm['ATM_ID']}: {e}")
+#         logger.info("Skipping to the next ATM.")
+
+# def disconnect_from_atm(atm):
+#     try:
+#         # Check if there is a previous connection
+#         if atm["ATM_ID"] in previous_connections:
+#             subprocess.run(['net', 'use', fr'\\{atm["ATM_IP"]}\IPC$', '/delete'], check=True, timeout=5)  # Set a shorter timeout of 5 seconds
+#             logger.info(f"Disconnected from ATM {atm['ATM_ID']}")
+#         else:
+#             logger.info(f"No previous connection found for ATM {atm['ATM_ID']}. Skipping force disconnect.")
+
+#     except subprocess.CalledProcessError as e:
+#         logger.error(f"Error disconnecting from ATM {atm['ATM_ID']} ({atm['ATM_IP']}): {e}")
+
+# def copy_file_from_atm(atm):
+#     try:
+#         source_file = fr'\\{atm["ATM_IP"]}\{LOGS_FOLDER}\{LOG_FILE_NAME}'
+#         logger.debug(f"Source file path: {source_file}")
+
+#         # Destination folder based on ATM information
+#         destination_folder = os.path.join(
+#             BASE_DESTINATION_FOLDER,
+#             f'ATM_{atm["ATM_ID"]}_{atm["ATM_TYPE"]}_{atm["BRANCH_ID"]}_{atm["BRANCH_NAME"]}'
+#         )
+
+#         logger.debug(f"Destination folder path: {destination_folder}")
+
+#         if not os.path.exists(destination_folder):
+#             os.makedirs(destination_folder)
+
+#         destination_file = os.path.join(destination_folder, LOG_FILE_NAME)
+#         logger.debug(f"Destination file path: {destination_file}")
+
+#         shutil.copy2(source_file, destination_file)
+#         logger.info(f"File copied from {source_file} to {destination_file}")
+
+#     except Exception as e:
+#         logger.error(f"Error copying file: {e}")
+
+# def main():
+#     with open("atm_config.json", "r") as config_file:
+#         atm_list = json.load(config_file)
+
+#     for atm_info in atm_list:
+#         connect_to_atm(atm_info)
+#         if "ATM_ID" in atm_info and atm_info["ATM_ID"] in previous_connections:
+#             copy_file_from_atm(atm_info)
+#         disconnect_from_atm(atm_info)
+
+# if __name__ == "__main__":
+#     main()
+
+
 def is_valid_ip(ip):
     try:
         socket.inet_aton(ip)
@@ -20,43 +108,43 @@ def connect_to_atm(atm):
     try:
         # Quick IP validation
         if not is_valid_ip(atm["ATM_IP"]):
-            logger.error(f"Invalid IP address for ATM {atm['ATM_ID']}. Skipping.")
+            logger.error(f"Invalid IP address for ATM {atm['ATM_Terminal_Id']}. Skipping.")
             return
 
         # Check if there is a previous connection
-        if atm["ATM_ID"] in previous_connections:
-            logger.info(f"Using existing connection for ATM {atm['ATM_ID']}")
+        if atm["ATM_Terminal_Id"] in previous_connections:
+            logger.info(f"Using existing connection for ATM {atm['ATM_Terminal_Id']}")
         else:
             net_use_command = [
-                'net', 'use', fr'\\{atm["ATM_IP"]}\IPC$', f'/user:{atm["Domain"]}\\{atm["Username"]}', atm["Password"]
+                'net', 'use', fr'\\{atm["ATM_IP"]}\IPC$', f'/user:{atm["Username"]}', atm["Password"]
             ]
             subprocess.run(net_use_command, check=True, timeout=5)  # Set a shorter timeout of 5 seconds
-            logger.info(f"Connected to ATM {atm['ATM_ID']}")
+            logger.info(f"Connected to ATM {atm['ATM_Terminal_Id']}")
 
             # Save the connection in the dictionary
-            previous_connections[atm["ATM_ID"]] = True
+            previous_connections[atm["ATM_Terminal_Id"]] = True
 
     except subprocess.CalledProcessError as e:
-        logger.error(f"Authentication failed for ATM {atm['ATM_ID']} ({atm['ATM_IP']}): {e}")
+        logger.error(f"Authentication failed for ATM {atm['ATM_Terminal_Id']} ({atm['ATM_IP']}): {e}")
         logger.info("Skipping to the next ATM.")
     except subprocess.TimeoutExpired as e:
-        logger.error(f"Authentication failed or timeout for ATM {atm['ATM_ID']} ({atm['ATM_IP']}): {e}")
+        logger.error(f"Authentication failed or timeout for ATM {atm['ATM_Terminal_Id']} ({atm['ATM_IP']}): {e}")
         logger.info("Skipping to the next ATM.")
     except Exception as e:
-        logger.error(f"Error connecting to ATM {atm['ATM_ID']}: {e}")
+        logger.error(f"Error connecting to ATM {atm['ATM_Terminal_Id']}: {e}")
         logger.info("Skipping to the next ATM.")
 
 def disconnect_from_atm(atm):
     try:
         # Check if there is a previous connection
-        if atm["ATM_ID"] in previous_connections:
+        if atm["ATM_Terminal_Id"] in previous_connections:
             subprocess.run(['net', 'use', fr'\\{atm["ATM_IP"]}\IPC$', '/delete'], check=True, timeout=5)  # Set a shorter timeout of 5 seconds
-            logger.info(f"Disconnected from ATM {atm['ATM_ID']}")
+            logger.info(f"Disconnected from ATM {atm['ATM_Terminal_Id']}")
         else:
-            logger.info(f"No previous connection found for ATM {atm['ATM_ID']}. Skipping force disconnect.")
+            logger.info(f"No previous connection found for ATM {atm['ATM_Terminal_Id']}. Skipping force disconnect.")
 
     except subprocess.CalledProcessError as e:
-        logger.error(f"Error disconnecting from ATM {atm['ATM_ID']} ({atm['ATM_IP']}): {e}")
+        logger.error(f"Error disconnecting from ATM {atm['ATM_Terminal_Id']} ({atm['ATM_IP']}): {e}")
 
 def copy_file_from_atm(atm):
     try:
@@ -66,7 +154,7 @@ def copy_file_from_atm(atm):
         # Destination folder based on ATM information
         destination_folder = os.path.join(
             BASE_DESTINATION_FOLDER,
-            f'ATM_{atm["ATM_ID"]}_{atm["ATM_TYPE"]}_{atm["BRANCH_ID"]}_{atm["BRANCH_NAME"]}'
+            f'ATM-{atm["ATM_Location"]}-{atm["ATM_Terminal_Id"]}'
         )
 
         logger.debug(f"Destination folder path: {destination_folder}")
@@ -89,9 +177,13 @@ def main():
 
     for atm_info in atm_list:
         connect_to_atm(atm_info)
-        if "ATM_ID" in atm_info and atm_info["ATM_ID"] in previous_connections:
+        if "ATM_Terminal_Id" in atm_info and atm_info["ATM_Terminal_Id"] in previous_connections:
             copy_file_from_atm(atm_info)
-        disconnect_from_atm(atm_info)
+            disconnect_from_atm(atm_info)  # Disconnect after copying file
+        else:
+            disconnect_from_atm(atm_info)  # Disconnect if no copying is needed
+
+
 
 if __name__ == "__main__":
     main()
