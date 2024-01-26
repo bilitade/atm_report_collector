6eec10283a4d08727c5f7db0fba34fdbe888017d
Bilisuma <coop@DIG-BLP-10.coopbank.local>
2024-01-26 15:58:21 +0300

    live log implemented

--- gui.py
+++ gui.py
@@ -1,4 +1,3 @@
-# Import necessary modules
 import tkinter as tk
 from tkinter import ttk
 from queue import Queue
@@ -11,137 +10,116 @@ import queue
 
 class ATMLogGrapperApp:
     def __init__(self, root, width, height):
-        # Initialize GUI elements
         self.root = root
         self.root.title("ATM LOGO Grapper")
         self.root.geometry(f"{width}x{height}")
         self.root.resizable(False, False)
-
-        # Create main frame
         self.main_frame = ttk.Frame(root)
         self.main_frame.pack(fill=tk.BOTH, expand=True, padx=20, pady=20)
-
-        # Initialize logging queue
         self.log_queue = Queue()
-
-        # Create logging handler
         gui_log_handler = GUIConsoleLogHandler(self.log_queue)
         gui_log_handler.setLevel(logging.DEBUG)
-
-        # Create logging formatter
         formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%I-%M-%S%p %Y-%m-%d')
         gui_log_handler.setFormatter(formatter)
-
-        # Add logging handler to root logger
         logging.getLogger().addHandler(gui_log_handler)
-
-        # Start thread to update console log
         self.update_console_log_thread = Thread(target=self.update_console_log)
         self.update_console_log_thread.daemon = True
         self.update_console_log_thread.start()
-
-        # Create GUI elements
+        self.start_time = 0  # Initialize start_time
         self.create_gui_elements()
 
-     
-
     def run_script(self):
-        # Get paths and configurations from GUI elements
+        self.start_time = time.time()  # Update start_time
         atm_config_path = self.atmconfig_path_entry.get()
         shared_folder_name = self.shared_folder_entry.get()
         logs_path = self.logs_path_entry.get()
-        
-        # Record the start time
-        start_time = time.time()
-
-
         # Start the script execution in a separate thread
         self.script_execution_thread = Thread(target=self.execute_script, args=(atm_config_path, shared_folder_name, logs_path))
         self.script_execution_thread.start()
-         # Wait for the script execution thread to complete
-        self.script_execution_thread.join()
+        
+        # Update the start time label
+        self.start_time_label.config(text=f"Start Time: {time.strftime('%H:%M:%S', time.localtime(self.start_time))}")
 
-        # Record the end time
-        end_time = time.time()
+ 
 
-        # Calculate the execution time
-        execution_time = end_time - start_time
-
-        # Update the execution time label on the GUI
-        self.execution_time_label.config(text=f"Execution Time: {execution_time:.2f} seconds")
-
-
-
-    
     def create_gui_elements(self):
-        # Logo
-        self.logo_image = tk.PhotoImage(file="logo.png")
-        self.logo_label = ttk.Label(self.main_frame, image=self.logo_image)
-        self.logo_label.grid(row=0, column=0, columnspan=2, pady=10)
+                # Logo
+                self.logo_image = tk.PhotoImage(file="logo.png")
+                self.logo_label = ttk.Label(self.main_frame, image=self.logo_image)
+                self.logo_label.grid(row=0, column=0, columnspan=2, pady=10)
 
-        # Left side
-        left_frame = ttk.Frame(self.main_frame)
-        left_frame.grid(row=1, column=0, padx=10, sticky=tk.W)
+                # Left side
+                left_frame = ttk.Frame(self.main_frame)
+                left_frame.grid(row=1, column=0, padx=10, sticky=tk.W)
 
-        ttk.Label(left_frame, text="ATMConfig.json file path:").grid(row=0, column=0, sticky=tk.W, pady=(10, 5))
-        self.atmconfig_path_entry = ttk.Entry(left_frame)
-        self.atmconfig_path_entry.grid(row=0, column=1, sticky=tk.W, pady=(10, 5))
-        self.atmconfig_path_entry.insert(0, ".\\atm_config.json")
+                ttk.Label(left_frame, text="ATMConfig.json file path:").grid(row=0, column=0, sticky=tk.W, pady=(10, 5))
+                self.atmconfig_path_entry = ttk.Entry(left_frame)
+                self.atmconfig_path_entry.grid(row=0, column=1, sticky=tk.W, pady=(10, 5))
+                self.atmconfig_path_entry.insert(0, ".\\atm_config.json")
 
-        ttk.Label(left_frame, text="Shared folder name of all ATMs:").grid(row=1, column=0, sticky=tk.W, pady=(10, 5))
-        self.shared_folder_entry = ttk.Entry(left_frame)
-        self.shared_folder_entry.grid(row=1, column=1, sticky=tk.W, pady=(10, 5))
-        self.shared_folder_entry.insert(0, "EJLogs")
+                ttk.Label(left_frame, text="Shared folder name of all ATMs:").grid(row=1, column=0, sticky=tk.W, pady=(10, 5))
+                self.shared_folder_entry = ttk.Entry(left_frame)
+                self.shared_folder_entry.grid(row=1, column=1, sticky=tk.W, pady=(10, 5))
+                self.shared_folder_entry.insert(0, "EJLogs")
 
-        ttk.Label(left_frame, text="Path to copy logs:").grid(row=2, column=0, sticky=tk.W, pady=(10, 5))
-        self.logs_path_entry = ttk.Entry(left_frame)
-        self.logs_path_entry.grid(row=2, column=1, sticky=tk.W, pady=(10, 5))
-        self.logs_path_entry.insert(0, "C:\\ATMLogs")
+                ttk.Label(left_frame, text="Path to copy logs:").grid(row=2, column=0, sticky=tk.W, pady=(10, 5))
+                self.logs_path_entry = ttk.Entry(left_frame)
+                self.logs_path_entry.grid(row=2, column=1, sticky=tk.W, pady=(10, 5))
+                self.logs_path_entry.insert(0, "C:\\ATMLogs")
 
-        self.run_button = ttk.Button(left_frame, text="Run Script", command=self.run_script)
-        self.run_button.grid(row=3, column=0, columnspan=2, pady=10)
+                self.run_button = ttk.Button(left_frame, text="Run Script", command=self.run_script)
+                self.run_button.grid(row=3, column=0, columnspan=2, pady=10)
 
-        # Right side
-        right_frame = ttk.Frame(self.main_frame)
-        right_frame.grid(row=1, column=1, padx=10, sticky=tk.W)
+                # Right side
+                right_frame = ttk.Frame(self.main_frame)
+                right_frame.grid(row=1, column=1, padx=10, sticky=tk.W)
 
-        # Create the console log text widget
-        self.console_log_text = tk.Text(right_frame, height=20, wrap=tk.NONE)
-        self.console_log_text.grid(row=0, column=0, padx=10, pady=5, sticky=tk.NSEW)
+                # Create the console log text widget
+                self.console_log_text = tk.Text(right_frame, height=20, wrap=tk.NONE)
+                self.console_log_text.grid(row=0, column=0, padx=10, pady=5, sticky=tk.NSEW)
 
-         # Execution time label
-        self.execution_time_label = ttk.Label(right_frame, text="Execution Time: 0 seconds")
-        self.execution_time_label.grid(row=1, column=0, columnspan=2, pady=5)
+                # Add a scrollbar for the console log text widget
+                console_scrollbar = ttk.Scrollbar(right_frame, orient=tk.VERTICAL, command=self.console_log_text.yview)
+                console_scrollbar.grid(row=0, column=1, sticky=tk.NS)
+                self.console_log_text.config(yscrollcommand=console_scrollbar.set)
 
+                # Execution time label
+                self.execution_time_label = ttk.Label(right_frame, text="Execution Time: 0 seconds")
+                self.execution_time_label.grid(row=1, column=0, columnspan=2, pady=5)
+
+                # Start time label
+                self.start_time_label = ttk.Label(right_frame, text="Start Time: -")
+                self.start_time_label.grid(row=2, column=0, columnspan=2, pady=5)
+
+     
     def execute_script(self, atm_config_path, shared_folder_name, logs_path):
         try:
             # Call the backend function with the provided parameters
             run_script(atm_config_path, shared_folder_name, logs_path)
         except Exception as e:
             logging.exception("An error occurred while running the script")
+        finally:
+            # Record the end time
+            end_time = time.time()
 
-    def cancel_operation(self):
-        # Stop the script execution thread if it is running
-        if hasattr(self, 'script_execution_thread') and self.script_execution_thread.is_alive():
-            logging.warning("Cancelling operation...")
-            # Add your cancellation logic here
-            # For example, you can set a flag to indicate cancellation
-        else:
-            logging.info("No operation to cancel.")
+            # Calculate the execution time
+            execution_time = end_time - self.start_time
+
+            # Update the execution time label on the GUI
+            self.execution_time_label.config(text=f"Execution Time: {execution_time:.2f} seconds")
 
     def update_console_log(self):
         while True:
             try:
-                log_message = self.log_queue.get(timeout=0.1)  # Check for new log messages every 0.1 seconds
+                log_message = self.log_queue.get(timeout=0.1)
                 self.console_log_text.insert(tk.END, log_message + '\n')
-                self.console_log_text.see(tk.END)  # Auto-scroll to the bottom of the text widget
-            except queue.Empty:  # Use queue.Empty to handle the exception
+                self.console_log_text.see(tk.END)
+            except queue.Empty:
                 pass
+
 if __name__ == "__main__":
-    # Set custom resolution
     custom_width = 1000
     custom_height = 600
-
     root = tk.Tk()
     app = ATMLogGrapperApp(root, custom_width, custom_height)
     root.mainloop()
